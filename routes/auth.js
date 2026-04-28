211b2592f53641477dd9a6d1956fdc470fac8f96
mehmetpektass <pektasmehmett1184@gmail.com>
2026-04-28 23:47:15 +0300

    .

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ const User = require('../models/User');
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: process.env.GOOGLE_CALLBACK_URL || 'http://localhost:8000/api/auth/google/callback',
+  callbackURL: process.env.GOOGLE_CALLBACK_URL,
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ email: profile.emails[0].value });
